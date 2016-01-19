a1a76e58c0bb1657c634940464409671822a511c
tibbi <tibor@kaputa.sk>
2016-01-19 22:16:51 +0100

    lets listen for clicks from headset buttons too - 1 click - play/pause, 2 - next, 3 - previous

---
+++ app/src/main/java/musicplayer/simplemobiletools/com/RemoteControlReceiver.java
@@ -0,0 +1,50 @@
+package musicplayer.simplemobiletools.com;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.view.KeyEvent;
+
+import com.squareup.otto.Bus;
+
+public class RemoteControlReceiver extends BroadcastReceiver {
+    private static final int MAX_CLICK_DURATION = 700;
+    private static int clicksCnt;
+    private static Handler handler = new Handler();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_MEDIA_BUTTON.equals(intent.getAction())) {
+            final KeyEvent event = intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+            if (event.getAction() == KeyEvent.ACTION_UP && KeyEvent.KEYCODE_HEADSETHOOK == event.getKeyCode()) {
+                clicksCnt++;
+
+                handler.removeCallbacks(runnable);
+                if (clicksCnt >= 3) {
+                    handler.post(runnable);
+                } else {
+                    handler.postDelayed(runnable, MAX_CLICK_DURATION);
+                }
+            }
+        }
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            if (clicksCnt == 0)
+                return;
+
+            final Bus bus = BusProvider.getInstance();
+            if (clicksCnt == 1) {
+                bus.post(new Events.PlayPauseSong());
+            } else if (clicksCnt == 2) {
+                bus.post(new Events.NextSong());
+            } else {
+                bus.post(new Events.PreviousSong());
+            }
+            clicksCnt = 0;
+        }
+    };
+}
