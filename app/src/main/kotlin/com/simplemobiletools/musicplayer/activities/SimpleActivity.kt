d2cd7b687e945b9f0c7497bfc728c90b903dd8a7
tibbi <tibor@kaputa.sk>
2017-01-06 23:28:03 +0100

    remove some styles

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/activities/SimpleActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/activities/SimpleActivity.kt
@@ -3,16 +3,13 @@ package com.simplemobiletools.musicplayer.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
-
 import com.simplemobiletools.musicplayer.helpers.Config
-import com.simplemobiletools.musicplayer.R
 
 open class SimpleActivity : AppCompatActivity() {
     lateinit var mConfig: Config
 
     override fun onCreate(savedInstanceState: Bundle?) {
         mConfig = Config.newInstance(applicationContext)
-        setTheme(if (mConfig.isDarkTheme) R.style.AppTheme_Dark else R.style.AppTheme)
         super.onCreate(savedInstanceState)
     }
 
