a3ea4ec5b6f295bb9239bb5f3ab88f4065b8b0c5
tibbi <tibor@kaputa.sk>
2017-11-04 22:25:49 +0100

    remove a redundant override

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/activities/SimpleActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/activities/SimpleActivity.kt
@@ -1,10 +1,6 @@
 package com.simplemobiletools.musicplayer.activities
 
-import android.os.Bundle
 import com.simplemobiletools.commons.activities.BaseSimpleActivity
 
 open class SimpleActivity : BaseSimpleActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 }
