b6b8e71ec4ca7ab9d0bce23e4f0592fbd9c5f722
tibbi <tibor@kaputa.sk>
2017-11-30 00:07:30 +0100

    adding LeakCanary and BaseSplashActivity

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/App.kt
@@ -2,10 +2,19 @@ package com.simplemobiletools.musicplayer
 
 import android.app.Application
 import com.facebook.stetho.Stetho
+import com.simplemobiletools.musicplayer.BuildConfig.USE_LEAK_CANARY
+import com.squareup.leakcanary.LeakCanary
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (USE_LEAK_CANARY) {
+            if (LeakCanary.isInAnalyzerProcess(this)) {
+                return
+            }
+            LeakCanary.install(this)
+        }
+
         Stetho.initializeWithDefaults(this)
     }
 }
