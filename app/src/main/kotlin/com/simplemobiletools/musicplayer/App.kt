461a98cd9f83e5a2f5efb5c37a8da091fbbe5313
tibbi <tibor@kaputa.sk>
2017-12-01 20:31:37 +0100

    initialize Stetho only in debug build

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/App.kt
@@ -3,19 +3,18 @@ package com.simplemobiletools.musicplayer
 import android.app.Application
 import com.facebook.stetho.Stetho
 import com.simplemobiletools.commons.extensions.checkUseEnglish
-import com.simplemobiletools.musicplayer.BuildConfig.USE_LEAK_CANARY
 import com.squareup.leakcanary.LeakCanary
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        if (USE_LEAK_CANARY) {
+        if (BuildConfig.DEBUG) {
             if (LeakCanary.isInAnalyzerProcess(this)) {
                 return
             }
             LeakCanary.install(this)
+            Stetho.initializeWithDefaults(this)
         }
         checkUseEnglish()
-        Stetho.initializeWithDefaults(this)
     }
 }
