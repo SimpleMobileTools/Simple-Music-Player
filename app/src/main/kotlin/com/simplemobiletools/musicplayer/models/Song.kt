712dce12c72653cabb10fad1503c821b7e2a7148
tibbi <tibor@kaputa.sk>
2018-02-08 12:36:37 +0100

    move <unknown> title or artist to the bottom at sorting

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/models/Song.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/models/Song.kt
@@ -1,5 +1,6 @@
 package com.simplemobiletools.musicplayer.models
 
+import android.provider.MediaStore
 import com.simplemobiletools.commons.extensions.getFormattedDuration
 import com.simplemobiletools.commons.helpers.SORT_DESCENDING
 import com.simplemobiletools.musicplayer.helpers.SORT_BY_ARTIST
@@ -10,13 +11,29 @@ import java.io.Serializable
 data class Song(val id: Long, var title: String, var artist: String, var path: String, val duration: Int, val album: String) : Serializable, Comparable<Song> {
     companion object {
         private const val serialVersionUID = 6717978793256842245L
-        var sorting: Int = 0
+        var sorting = 0
     }
 
     override fun compareTo(other: Song): Int {
         var res = when {
-            sorting and SORT_BY_TITLE != 0 -> title.toLowerCase().compareTo(other.title.toLowerCase())
-            sorting and SORT_BY_ARTIST != 0 -> artist.toLowerCase().compareTo(other.artist.toLowerCase())
+            sorting and SORT_BY_TITLE != 0 -> {
+                if (title == MediaStore.UNKNOWN_STRING && other.title != MediaStore.UNKNOWN_STRING) {
+                    1
+                } else if (title != MediaStore.UNKNOWN_STRING && other.title == MediaStore.UNKNOWN_STRING) {
+                    -1
+                } else {
+                    title.toLowerCase().compareTo(other.title.toLowerCase())
+                }
+            }
+            sorting and SORT_BY_ARTIST != 0 -> {
+                if (artist == MediaStore.UNKNOWN_STRING && other.artist != MediaStore.UNKNOWN_STRING) {
+                    1
+                } else if (artist != MediaStore.UNKNOWN_STRING && other.artist == MediaStore.UNKNOWN_STRING) {
+                    -1
+                } else {
+                    artist.toLowerCase().compareTo(other.artist.toLowerCase())
+                }
+            }
             sorting and SORT_BY_PATH != 0 -> path.toLowerCase().compareTo(other.path.toLowerCase())
             else -> when {
                 duration == other.duration -> 0
@@ -28,6 +45,7 @@ data class Song(val id: Long, var title: String, var artist: String, var path: S
         if (sorting and SORT_DESCENDING != 0) {
             res *= -1
         }
+
         return res
     }
 
