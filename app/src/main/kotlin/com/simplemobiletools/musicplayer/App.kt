137d3bea3003f8ec2fee51f8ddea55183d253af0
tibbi <tibor@kaputa.sk>
2018-09-10 23:48:05 +0200

    updating Commons, kotlin, sdk

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/App.kt
@@ -2,17 +2,10 @@ package com.simplemobiletools.musicplayer
 
 import android.app.Application
 import com.simplemobiletools.commons.extensions.checkUseEnglish
-import com.squareup.leakcanary.LeakCanary
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            if (LeakCanary.isInAnalyzerProcess(this)) {
-                return
-            }
-            LeakCanary.install(this)
-        }
         checkUseEnglish()
     }
 }
