4f3de9a9ad1d792e560f97ef940cb19da21ba824
tibbi <tibor@kaputa.sk>
2020-05-06 18:37:46 +0200

    adding a crashfix at actionmenu item checking

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/adapters/SongAdapter.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/adapters/SongAdapter.kt
@@ -93,7 +93,7 @@ class SongAdapter(activity: SimpleActivity, var songs: ArrayList<Song>, val list
 
     override fun prepareActionMode(menu: Menu) {
         menu.apply {
-            findItem(R.id.cab_rename).isVisible = isOneItemSelected() && !getSelectedSongs().first().path.startsWith("content://")
+            findItem(R.id.cab_rename).isVisible = isOneItemSelected() && getSelectedSongs().firstOrNull()?.path?.startsWith("content://") != true
         }
     }
 
