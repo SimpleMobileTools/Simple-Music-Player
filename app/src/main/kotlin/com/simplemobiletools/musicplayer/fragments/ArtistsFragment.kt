c53418719ab96abbe05228cd62ee29225d98c86b
tibbi <tibor@kaputa.sk>
2020-09-30 16:26:47 +0200

    adding Artist fetching

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/fragments/ArtistsFragment.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/fragments/ArtistsFragment.kt
@@ -1,9 +1,55 @@
 package com.simplemobiletools.musicplayer.fragments
 
+import android.app.Activity
 import android.content.Context
+import android.provider.MediaStore.Audio
 import android.util.AttributeSet
+import com.simplemobiletools.commons.extensions.getIntValue
+import com.simplemobiletools.commons.extensions.getStringValue
+import com.simplemobiletools.commons.extensions.showErrorToast
+import com.simplemobiletools.commons.helpers.ensureBackgroundThread
 import com.simplemobiletools.musicplayer.activities.SimpleActivity
+import com.simplemobiletools.musicplayer.models.Artist
 
 class ArtistsFragment(context: Context, attributeSet: AttributeSet) : MyViewPagerFragment(context, attributeSet) {
-    override fun setupFragment(activity: SimpleActivity) {}
+    override fun setupFragment(activity: SimpleActivity) {
+        getArtists(activity) {
+
+        }
+    }
+
+    private fun getArtists(activity: Activity, callback: (artists: ArrayList<Artist>) -> Unit) {
+        ensureBackgroundThread {
+            val artists = ArrayList<Artist>()
+            val uri = Audio.Artists.EXTERNAL_CONTENT_URI
+            val projection = arrayOf(
+                Audio.Artists._ID,
+                Audio.Artists.ARTIST,
+                Audio.ArtistColumns.NUMBER_OF_ALBUMS,
+                Audio.ArtistColumns.NUMBER_OF_TRACKS
+            )
+
+            try {
+                val cursor = activity.contentResolver.query(uri, projection, null, null, null)
+                cursor?.use {
+                    if (cursor.moveToFirst()) {
+                        do {
+                            val id = cursor.getIntValue(Audio.Artists._ID)
+                            val title = cursor.getStringValue(Audio.Artists.ARTIST)
+                            val albumCnt = cursor.getIntValue(Audio.ArtistColumns.NUMBER_OF_ALBUMS)
+                            val trackCnt = cursor.getIntValue(Audio.ArtistColumns.NUMBER_OF_TRACKS)
+                            val artist = Artist(id, title, albumCnt, trackCnt)
+                            artists.add(artist)
+                        } while (cursor.moveToNext())
+                    }
+                }
+            } catch (e: Exception) {
+                activity.showErrorToast(e)
+            }
+
+            activity.runOnUiThread {
+                callback(artists)
+            }
+        }
+    }
 }
---
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/models/Artist.kt
@@ -0,0 +1,3 @@
+package com.simplemobiletools.musicplayer.models
+
+data class Artist(var id: Int, val title: String, val albumCnt: Int, val trackCnt: Int)
