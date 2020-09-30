e0e163e4c8271062843642b33decaa843b8a244f
tibbi <tibor@kaputa.sk>
2020-09-30 15:35:38 +0200

    fill the playlists tab

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/fragments/PlaylistsFragment.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/fragments/PlaylistsFragment.kt
@@ -2,8 +2,25 @@ package com.simplemobiletools.musicplayer.fragments
 
 import android.content.Context
 import android.util.AttributeSet
+import com.simplemobiletools.commons.helpers.ensureBackgroundThread
 import com.simplemobiletools.musicplayer.activities.SimpleActivity
+import com.simplemobiletools.musicplayer.adapters.PlaylistsAdapter
+import com.simplemobiletools.musicplayer.extensions.playlistChanged
+import com.simplemobiletools.musicplayer.extensions.playlistDAO
+import com.simplemobiletools.musicplayer.models.Playlist
+import kotlinx.android.synthetic.main.fragment_playlists.view.*
 
 class PlaylistsFragment(context: Context, attributeSet: AttributeSet) : MyViewPagerFragment(context, attributeSet) {
-    override fun setupFragment(activity: SimpleActivity) {}
+    override fun setupFragment(activity: SimpleActivity) {
+        ensureBackgroundThread {
+            val playlists = activity.playlistDAO.getAll() as ArrayList<Playlist>
+            activity.runOnUiThread {
+                PlaylistsAdapter(activity, playlists, null, playlists_list) {
+                    activity.playlistChanged((it as Playlist).id)
+                }.apply {
+                    playlists_list.adapter = this
+                }
+            }
+        }
+    }
 }
