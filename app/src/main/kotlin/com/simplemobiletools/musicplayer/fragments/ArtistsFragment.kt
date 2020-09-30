d7e7077aa31104046093793dd341a00692922292
tibbi <tibor@kaputa.sk>
2020-09-30 15:37:26 +0200

    fixing a typo at Artists

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/fragments/ArtistsFragment.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/fragments/ArtistsFragment.kt
@@ -4,6 +4,6 @@ import android.content.Context
 import android.util.AttributeSet
 import com.simplemobiletools.musicplayer.activities.SimpleActivity
 
-class ArtistssFragment(context: Context, attributeSet: AttributeSet) : MyViewPagerFragment(context, attributeSet) {
+class ArtistsFragment(context: Context, attributeSet: AttributeSet) : MyViewPagerFragment(context, attributeSet) {
     override fun setupFragment(activity: SimpleActivity) {}
 }
