48d18cf881a2b2b6f428d0a44cea269bd78bcde9
tibbi <tibor@kaputa.sk>
2020-10-19 21:53:58 +0200

    list albums at the appropriate tab

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/fragments/AlbumsFragment.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/fragments/AlbumsFragment.kt
@@ -1,12 +1,18 @@
 package com.simplemobiletools.musicplayer.fragments
 
 import android.content.Context
+import android.content.Intent
 import android.util.AttributeSet
+import com.google.gson.Gson
 import com.simplemobiletools.commons.adapters.MyRecyclerViewAdapter
+import com.simplemobiletools.commons.helpers.AlphanumericComparator
 import com.simplemobiletools.commons.helpers.ensureBackgroundThread
 import com.simplemobiletools.musicplayer.activities.SimpleActivity
+import com.simplemobiletools.musicplayer.activities.TracksActivity
+import com.simplemobiletools.musicplayer.adapters.AlbumsAdapter
 import com.simplemobiletools.musicplayer.extensions.getAlbumsSync
 import com.simplemobiletools.musicplayer.extensions.getArtistsSync
+import com.simplemobiletools.musicplayer.helpers.ALBUM
 import com.simplemobiletools.musicplayer.models.Album
 import kotlinx.android.synthetic.main.fragment_albums.view.*
 
@@ -19,6 +25,24 @@ class AlbumsFragment(context: Context, attributeSet: AttributeSet) : MyViewPager
             val artists = activity.getArtistsSync()
             artists.forEach { artist ->
                 albums.addAll(activity.getAlbumsSync(artist))
+            }
+
+            albums.sortWith { o1, o2 -> AlphanumericComparator().compare(o1.title.toLowerCase(), o2.title.toLowerCase()) }
+
+            activity.runOnUiThread {
+                val adapter = AlbumsAdapter(activity, albums, albums_list, albums_fastscroller) {
+                    Intent(activity, TracksActivity::class.java).apply {
+                        putExtra(ALBUM, Gson().toJson(it))
+                        activity.startActivity(this)
+                    }
+                }.apply {
+                    albums_list.adapter = this
+                }
+
+                albums_fastscroller.setViews(albums_list) {
+                    val album = adapter.albums.getOrNull(it)
+                    albums_fastscroller.updateBubbleText(album?.title ?: "")
+                }
             }
         }
 
