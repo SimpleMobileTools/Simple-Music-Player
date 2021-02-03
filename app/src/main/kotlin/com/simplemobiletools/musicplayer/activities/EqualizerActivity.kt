8ea631713c16d146a59da203dc88776971889e58
tibbi <tibor@kaputa.sk>
2021-02-03 21:40:06 +0100

    properley format the frequency shown under the bands

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/activities/EqualizerActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/activities/EqualizerActivity.kt
@@ -8,6 +8,7 @@ import android.view.Menu
 import com.simplemobiletools.commons.extensions.updateTextColors
 import com.simplemobiletools.musicplayer.R
 import kotlinx.android.synthetic.main.activity_equalizer.*
+import java.text.DecimalFormat
 
 class EqualizerActivity : SimpleActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,5 +29,22 @@ class EqualizerActivity : SimpleActivity() {
         val equalizer = Equalizer(0, player.audioSessionId)
         equalizer_label_bottom.text = (equalizer.bandLevelRange[0] / 100).toString()
         equalizer_label_top.text = "+${equalizer.bandLevelRange[1] / 100}"
+
+        val bands = equalizer.numberOfBands
+        for (i in 0 until bands) {
+            val frequency = equalizer.getCenterFreq(i.toShort()) / 1000
+            val formatted = formatFrequency(frequency)
+        }
+    }
+
+    // copypasted  from the file size formatter, should be simplified
+    private fun formatFrequency(value: Int): String {
+        if (value <= 0) {
+            return "0 Hz"
+        }
+
+        val units = arrayOf("Hz", "kHz", "gHz")
+        val digitGroups = (Math.log10(value.toDouble()) / Math.log10(1000.0)).toInt()
+        return "${DecimalFormat("#,##0.#").format(value / Math.pow(1000.0, digitGroups.toDouble()))} ${units[digitGroups]}"
     }
 }
