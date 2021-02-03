eebc1f66cd3a24d98856e841a10d4b3cb9eec0e2
tibbi <tibor@kaputa.sk>
2021-02-03 20:11:19 +0100

    adding the vertical seekbars at the equalizer activity

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/activities/EqualizerActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/activities/EqualizerActivity.kt
@@ -2,12 +2,15 @@ package com.simplemobiletools.musicplayer.activities
 
 import android.os.Bundle
 import android.view.Menu
+import com.simplemobiletools.commons.extensions.updateTextColors
 import com.simplemobiletools.musicplayer.R
+import kotlinx.android.synthetic.main.activity_equalizer.*
 
 class EqualizerActivity : SimpleActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_equalizer)
+        updateTextColors(equalizer_holder)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/views/VerticalSeekBar.kt
@@ -0,0 +1,45 @@
+package com.simplemobiletools.musicplayer.views
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.appcompat.widget.AppCompatSeekBar
+
+class VerticalSeekBar : AppCompatSeekBar {
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(height, width, oldHeight, oldWidth)
+    }
+
+    @Synchronized
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(heightMeasureSpec, widthMeasureSpec)
+        setMeasuredDimension(measuredHeight, measuredWidth)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.rotate(-90f)
+        canvas.translate(-height.toFloat(), 0f)
+        super.onDraw(canvas)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE, MotionEvent.ACTION_UP -> {
+                progress = max - (max * event.y / height).toInt()
+                onSizeChanged(width, height, 0, 0)
+            }
+            MotionEvent.ACTION_CANCEL -> {
+            }
+        }
+        return true
+    }
+}
