71f59d5dcc7e59a96067a589f1230acb9425908a
tibbi <tibor@kaputa.sk>
2021-08-24 15:45:51 +0200

    tweaking some MarqueeTextView code style, no functionality change

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/views/MarqueeTextView.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/views/MarqueeTextView.kt
@@ -7,15 +7,22 @@ import android.util.AttributeSet
 import android.view.View
 import androidx.appcompat.widget.AppCompatTextView
 
+class MarqueeTextView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : AppCompatTextView(context!!, attrs, defStyleAttr),
+    View.OnLayoutChangeListener {
 
-class MarqueeTextView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) :
-    AppCompatTextView(context!!, attrs, defStyleAttr), View.OnLayoutChangeListener {
-    constructor(context: Context?) : this(context, null) {}
-    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0) {}
+    constructor(context: Context?) : this(context, null)
 
-    override fun isFocused(): Boolean {
-        return true
+    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    init {
+        setSingleLine()
+        ellipsize = TextUtils.TruncateAt.MARQUEE
+        marqueeRepeatLimit = -1
+        isSelected = true
+        addOnLayoutChangeListener(this)
     }
+
+    override fun isFocused() = true
 
     override fun onWindowFocusChanged(hasWindowFocus: Boolean) {
         if (hasWindowFocus) super.onWindowFocusChanged(hasWindowFocus)
@@ -25,22 +32,11 @@ class MarqueeTextView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int
         if (focused) super.onFocusChanged(focused, direction, previouslyFocusedRect)
     }
 
-    override fun onLayoutChange(
-        v: View?, left: Int, top: Int, right: Int, bottom: Int,
-        oldLeft: Int, oldTop: Int, oldRight: Int, oldBottom: Int
-    ) {
+    override fun onLayoutChange(v: View?, left: Int, top: Int, right: Int, bottom: Int, oldLeft: Int, oldTop: Int, oldRight: Int, oldBottom: Int) {
         val layoutParams = layoutParams
         layoutParams.height = bottom - top
         layoutParams.width = right - left
         removeOnLayoutChangeListener(this)
         setLayoutParams(layoutParams)
-    }
-
-    init {
-        setSingleLine()
-        ellipsize = TextUtils.TruncateAt.MARQUEE
-        marqueeRepeatLimit = -1
-        isSelected = true
-        addOnLayoutChangeListener(this)
     }
 }
