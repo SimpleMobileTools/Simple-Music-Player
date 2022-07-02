df25a051be152af0e30df26de19a61fc9437f25d
tibbi <tibor@kaputa.sk>
2022-07-02 15:47:19 +0200

    avoid showing some FAQ items in some cases

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/activities/MainActivity.kt
@@ -549,10 +549,13 @@ class MainActivity : SimpleActivity() {
             FAQItem(R.string.faq_1_title, R.string.faq_1_text),
             FAQItem(R.string.faq_1_title_commons, R.string.faq_1_text_commons),
             FAQItem(R.string.faq_4_title_commons, R.string.faq_4_text_commons),
-            FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons),
-            FAQItem(R.string.faq_6_title_commons, R.string.faq_6_text_commons),
             FAQItem(R.string.faq_9_title_commons, R.string.faq_9_text_commons)
         )
+
+        if (!resources.getBoolean(R.bool.hide_google_relations)) {
+            faqItems.add(FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons))
+            faqItems.add(FAQItem(R.string.faq_6_title_commons, R.string.faq_6_text_commons))
+        }
 
         startAboutActivity(R.string.app_name, licenses, BuildConfig.VERSION_NAME, faqItems, true)
     }
