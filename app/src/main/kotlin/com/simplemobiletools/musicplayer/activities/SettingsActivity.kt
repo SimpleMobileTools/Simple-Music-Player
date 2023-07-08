4cac11a50a6f6dbfd16a02716685adbec0aeacc9
Naveen <snaveen935@gmail.com>
2023-07-08 20:49:18 +0530

    Add `Scan files manually` preference

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/activities/SettingsActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/activities/SettingsActivity.kt
@@ -13,7 +13,9 @@ import com.simplemobiletools.musicplayer.dialogs.ManageVisibleTabsDialog
 import com.simplemobiletools.musicplayer.extensions.config
 import com.simplemobiletools.musicplayer.extensions.sendIntent
 import com.simplemobiletools.musicplayer.helpers.*
+import com.simplemobiletools.musicplayer.models.Events
 import kotlinx.android.synthetic.main.activity_settings.*
+import org.greenrobot.eventbus.EventBus
 import java.util.*
 import kotlin.system.exitProcess
 
@@ -41,6 +43,7 @@ class SettingsActivity : SimpleActivity() {
         setupManageShownTabs()
         setupSwapPrevNext()
         setupReplaceTitle()
+        setupManualScanning()
         setupGaplessPlayback()
         updateTextColors(settings_nested_scrollview)
 
@@ -132,6 +135,15 @@ class SettingsActivity : SimpleActivity() {
     private fun setupManageExcludedFolders() {
         settings_manage_excluded_folders_holder.setOnClickListener {
             startActivity(Intent(this, ExcludedFoldersActivity::class.java))
+        }
+    }
+
+    private fun setupManualScanning() {
+        settings_scan_files_manually.isChecked = config.scanFilesManually
+        settings_scan_files_manually_holder.setOnClickListener {
+            settings_scan_files_manually.toggle()
+            config.scanFilesManually = settings_scan_files_manually.isChecked
+            EventBus.getDefault().post(Events.RefreshFragments())
         }
     }
 
