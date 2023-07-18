646083d711fc91670cf9aed84d3e87271eceea58
Yashraj254 <yashr7651@gmail.com>
2023-07-18 19:55:04 +0530

    resolved all conflicts

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/extensions/Context.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/extensions/Context.kt
@@ -2,7 +2,6 @@ package com.simplemobiletools.musicplayer.extensions
 
 import android.annotation.SuppressLint
 import android.app.Application
-import android.content.ContentUris
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
